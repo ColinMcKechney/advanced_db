54b4a30f90ef17da203c3e4c3893ec51bb5a685e
Colin McKechney <l3gion@ColinLaptop>
2023-04-27 01:46:34 -0400

    basic web server functionality

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,49 @@ use rand::{prelude::Rng, distributions::Alphanumeric };
 use oracle::{Connection, Error};
 use log::{info, warn, error};
 use env_logger::Env;
+use actix_web::{web, get, post, web::Json, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
 
 static SQL_USERNAME: &str = "group09_user";
 static SQL_PASSWORD: &str = "group09_user";
 static SALT_LEN: usize = 16;
+static PORT: u16 = 8009;
 
-fn main() {
+
+#[derive(Default, Debug, Serialize, Deserialize, Clone)]
+struct Login {
+    net_id: String,
+    password: String,
+}
+
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     
     //init logging system
     let env = Env::default().filter_or("LOG_LEVEL", "info");
     env_logger::init_from_env(env);
 
+    let result = HttpServer::new( || {
+        App::new()
+            .service(index)
+            .service(login)
+            .service(homepage)
+            .service(plan_page)
+    })
+    .bind(("127.0.0.1", PORT))?
+    .run()
+    .await;
     //Temporary for testing purposes, should write something to make a random salt
     let username = "cmckechn";
     let password = "password";
 
     //proof of concept tests, create_user should fail in this instance because user was already
     //created
-    println!("{}",authenticate(username, password).unwrap());
-    create_user("test", "test_create", "test_first", "test_last").unwrap();
-    println!("{}", authenticate("test", "test_create").unwrap());
+    //println!("{}",authenticate(username, password).unwrap());
+    //create_user("test", "test_create", "test_first", "test_last").unwrap();
+    //println!("{}", authenticate("test", "test_create").unwrap());
+    Ok(())
 }
 
 
@@ -94,4 +117,26 @@ fn create_user(username: &str, password: &str, first_name: &str, last_name: &str
     conn.close()?;
     Ok(())
 
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[get("/login")]
+async fn login(json: Json<Login>) -> Result<String, actix_web::Error> {
+    Ok(format!("{} {}", json.net_id, json.password))
+}
+
+#[get("/{net_id}/home")]
+async fn homepage(path: web::Path<String>) -> impl Responder {
+    let net_id = path.into_inner();
+    HttpResponse::Ok().body(format!("You have reached the homepage of {} user", net_id) )
+}
+
+#[get("/{net_id}/plans")]
+async fn plan_page(path: web::Path<String>) -> impl Responder {
+    let net_id = path.into_inner();
+    HttpResponse::Ok().body(format!("You have reached the plan page of {}", net_id))
 }
