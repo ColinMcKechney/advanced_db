e32c1f9492b645016446e50922d0f278da219ce4
Colin McKechney <l3gion@ColinLaptop>
2023-04-27 23:59:14 -0400

    beginnings of authentication

---
+++ src/api/user.rs
@@ -0,0 +1,133 @@
+use sha2::{Sha256, Digest};
+use rand::{prelude::Rng, distributions::Alphanumeric };
+use oracle::{Connection, Error};
+use log::{info, warn, error};
+use actix_identity::Identity;
+use actix_web::{web, Responder, HttpRequest, HttpMessage, HttpResponse};
+use serde::{Deserialize, Serialize};
+
+
+static SQL_USERNAME: &str = "group09_user";
+static SQL_PASSWORD: &str = "group09_user";
+static SALT_LEN: usize = 16;
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+pub struct Entry {
+    net_id: String,
+    password: String
+}
+
+#[derive(Deserialize, Serialize, Debug, Default)]
+pub struct User {
+    id: String,
+    first_name: String,
+    last_name: String
+}
+
+
+pub async fn login(request: HttpRequest, body: web::Json<Entry>) -> impl Responder {
+    //TODO: finish login, for now will simply login for everyone
+    let net_id: &str = body.net_id.as_str();
+    let password: &str = body.password.as_str();
+    
+    //Identity::login(&request.extensions(), "User1".into());
+    println!("{:?}", request);
+    println!("{:?}", body);
+    match authenticate(net_id, password) {
+        Some(user) => {
+            Identity::login(&request.extensions(), net_id.into());
+            web::Json(user)
+        },
+        None => web::Json(User::default())
+    }
+}
+
+pub async fn logout(user: Identity) -> impl Responder {
+    user.logout();
+    HttpResponse::Ok()
+}
+
+fn authenticate(username: &str, password: &str) -> Option<User> {
+    
+    info!("Authenticating user: {}", username);
+
+    let conn = match Connection::connect(SQL_USERNAME,SQL_PASSWORD, ""){
+        Ok(c) => c,
+        Err(e) => {
+            error!("unable to open connection to server: {}", e);
+            return None;
+        }
+    };
+    let mut stmt = match conn.statement("select * from student where net_id = :1").build(){
+        Ok(s) => s,
+        Err(e) => {
+            error!("unable to build statement: {}", e);
+            return None;
+        }
+    };
+
+    let row = stmt.query_row_as::<(String, String, String, String, String)>(&[&username]).unwrap_or_default();
+    
+    let true_pword = row.3;
+    let salt = row.4;
+
+    let mut hasher = Sha256::new();
+    hasher.update(password);
+    hasher.update(salt);
+    let hash = hasher.finalize();
+
+    let mut tmp: String = String::new();
+    for value in hash{
+        tmp += &format!("{:x}", value); 
+    }
+    
+    conn.close().unwrap_or_default();
+
+
+    if true_pword.eq(&tmp) {
+        info!("User {} successfully authenticated", username);
+        Some( User { id: row.0, first_name: row.1,last_name: row.2} )
+    }
+    else{
+        warn!("User {} failed authentication", username);
+        None
+    }
+
+}
+
+
+
+
+fn create_user(username: &str, password: &str, first_name: &str, last_name: &str) -> Result<(), Error> {
+
+    info!("Creating user: {}", username); 
+    let conn = Connection::connect(SQL_USERNAME, SQL_PASSWORD, "")?;
+    let mut stmt = conn.statement("insert into student values(:net_id, :first_name, :last_name, :password, :salt)").build()?;
+
+    let salt: String = rand::thread_rng().sample_iter(&Alphanumeric).take(SALT_LEN).map(char::from).collect();
+    let mut hasher = Sha256::new();
+    hasher.update(&password);
+    hasher.update(&salt);
+    let hash = hasher.finalize();
+    
+    let mut hash_string = String::new();
+
+    for value in hash{
+        hash_string += &format!("{:x}", value);
+    }
+
+    match stmt.execute_named(&[("net_id", &username), ("first_name", &first_name), ("last_name", &last_name), ("password", &hash_string), ("salt", &salt)]) {
+        Ok(_) => {
+            info!("User {} successfully created", username); 
+            conn.commit()?;
+        },
+        Err(_) => {
+            warn!("Failed to create user {}", username);
+            conn.rollback()?;
+        }
+    };
+
+    conn.close()?;
+    Ok(())
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use log::{info, warn, error};
 use env_logger::Env;
-use actix_web::{web, get, post, web::Json, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, get, post, web::Json, App, HttpResponse, HttpServer, Responder, middleware, cookie::Key};
 use serde::{Deserialize, Serialize};
-mod security;
+use actix_cors::Cors;
+use actix_identity::IdentityMiddleware;
+use actix_session::{SessionMiddleware, storage::CookieSessionStore};
+mod api;
 
-static PORT: u16 = 8009;
+static PORT: u16 = 5000;
+const ALLOWED_ORIGIN: &str = "http://localhost";
 
 
 #[derive(Default, Debug, Serialize, Deserialize, Clone)]
@@ -21,46 +25,41 @@ async fn main() -> std::io::Result<()> {
     let env = Env::default().filter_or("LOG_LEVEL", "info");
     env_logger::init_from_env(env);
 
-    let _ = HttpServer::new( || {
+    let secret_key = Key::generate();
+
+    let _ = HttpServer::new(move || {
         App::new()
-            .service(index)
-            .service(login)
-            .service(homepage)
-            .service(plan_page)
-    })
-    .bind(("0.0.0.0", PORT))?
+            .wrap(middleware::Logger::default())
+            .wrap(
+                Cors::default()
+                .allowed_origin(ALLOWED_ORIGIN)
+                .allowed_methods(vec!["GET","POST","DELETE"])
+                .supports_credentials()
+                )
+            .wrap(IdentityMiddleware::default())
+            .wrap(SessionMiddleware::new(CookieSessionStore::default(), secret_key.clone()))
+            .service(
+                web::scope("/api")
+                .service(
+                    web::resource("/auth")
+                        .route(web::post().to(api::user::login))
+                        .route(web::delete().to(api::user::logout))
+                )
+                .route("/", web::get().to(api_index))
+            )
+            .route("/", web::get().to(index))
+    })    
+    .bind(("127.0.0.1", PORT))?
     .run()
     .await;
-    //Temporary for testing purposes, should write something to make a random salt
-    let username = "cmckechn";
-    let password = "password";
 
-    //proof of concept tests, create_user should fail in this instance because user was already
-    //created
-    security::authenticate(username, password).unwrap();
-    security::create_user("test", "test_create", "test_first", "test_last").unwrap();
-    security::authenticate("test", "test_create").unwrap();
     Ok(())
 }
 
-#[get("/")]
+async fn api_index() -> impl Responder {
+    HttpResponse::Ok().body("api")
+}
 async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    HttpResponse::Ok().body("/")
 }
 
-#[get("/login")]
-async fn login(json: Json<Login>) -> Result<String, actix_web::Error> {
-    Ok(format!("{} {}", json.net_id, json.password))
-}
-
-#[get("/{net_id}/home")]
-async fn homepage(path: web::Path<String>) -> impl Responder {
-    let net_id = path.into_inner();
-    HttpResponse::Ok().body(format!("You have reached the homepage of {} user", net_id) )
-}
-
-#[get("/{net_id}/plans")]
-async fn plan_page(path: web::Path<String>) -> impl Responder {
-    let net_id = path.into_inner();
-    HttpResponse::Ok().body(format!("You have reached the plan page of {}", net_id))
-}
